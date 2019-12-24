fe9a04bebdcbcedc3c69a4a1f47e3ddb29460f9c
karthit2s <59172469+karthit2s@users.noreply.github.com>
2019-12-24 19:28:21 +0000

    Mac addres
    Installing PDQ by using Mac address

---
+++ Mac.py
@@ -0,0 +1,73 @@
+
+
+import locale
+from dialog import Dialog
+import subprocess
+import tempfile
+import sys
+import os
+import psutil
+import pwd
+import time
+import shutil
+import hashlib
+import stat
+import colorama
+import argparse
+import re
+import requests
+import parser
+import ftplib
+import socket
+import fcntl
+import struct
+import shlex
+from sys import exit
+import base64
+import getpass
+import fileinput
+import os
+import sys
+import fileinput
+from dialog import Dialog
+
+
+def wired_pdq_mac():
+    textToSearch = 'PAX Computer Technology'
+    terminal_rows, terminal_cols = d.maxsize()
+    code, pdq_mac_address = d.inputbox("Enter the Mac address for Wired PDQ.",
+        height=max(terminal_rows - 10, 15),
+        width=max(terminal_cols - 16, 54))
+    if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", pdq_mac_address.lower()):
+        create_fusion_descovery_script()
+        with open('/home/client/fusion-linux/fusion_discover.py', 'r') as file :
+            filedata = file.read()
+            filedata = filedata.replace(textToSearch, pdq_mac_address)
+        with open('/home/client/fusion-linux/fusion_discover.py', 'w') as file:
+            file.write(filedata)
+        os.system("chmod +x /home/client/fusion-linux/fusion_discover.py")
+        os.system("chown client:client /home/client/fusion-linux/fusion_discover.py")
+    else:
+        print("Invalid Mac Address")
+
+def wireles_pdq_mac():
+    create_fusion_descovery_script()
+    textToSearch = 'Redpine Signals, Inc.'
+    terminal_rows, terminal_cols = d.maxsize()
+    code, pdq_mac_address = d.inputbox("Enter the Mac address for Wireless PDQ.",
+        height=max(terminal_rows - 10, 15),
+        width=max(terminal_cols - 16, 54))
+    if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", pdq_mac_address.lower()):
+        with open('/home/client/fusion-linux/fusion_discover.py', 'r') as file :
+            filedata = file.read()
+            filedata = filedata.replace(textToSearch, pdq_mac_address)
+        with open('/home/client/fusion-linux/fusion_discover.py', 'w') as file:
+            file.write(filedata)
+        os.system("chmod +x /home/client/fusion-linux/fusion_discover.py")
+        os.system("chown client:client /home/client/fusion-linux/fusion_discover.py")
+    else:
+        print("invalid Mac Address")
+
+d = Dialog(dialog="dialog")
+
+ps aux | grep optmany_service | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'
